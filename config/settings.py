23eb15269bdcec8960f9656e272e3486f0a13390
kabiri95 <erfankabiri337@gmail.com>
2023-01-29 17:07:47 +0330

    add comment

--- config/settings.py
+++ config/settings.py
@@ -127,10 +127,5 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend0'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'erfan.kabiri.djangotest@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Erfandjango1234'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+
 TIME_ZONE = 'America/New_York'
